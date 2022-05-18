41951538f88eaa6e303fe1bc5255b6b8911cd328
Niklas Widmann <niklas.widmann@zuehlke.com>
2022-05-18 17:48:51 +0200

    missing semicolon

--- lib/screens/search.dart
+++ lib/screens/search.dart
@@ -1,7 +1,4 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/widgets.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:gluttony_app/widgets/Layout.dart';
 
@@ -10,7 +7,7 @@ class SearchScreen extends HookWidget {
 
   @override
   Widget build(BuildContext context) {
-    final search = useState('')
+    final search = useState('');
 
     return Layout(body: Column(children: [TextFormField(autofocus: true)]));
   }
